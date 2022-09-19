bdaaa82de50247911152f0d0805770fc7b3b3dae
Kevin Holmgren <82142578+valu8kevin@users.noreply.github.com>
2022-09-19 11:37:56 +0200

    Updated ProcessingLite assignment

--- Course_01/Kevin_Holmgren_processinglite/Assets/Assignment1.cs
+++ Course_01/Kevin_Holmgren_processinglite/Assets/Assignment1.cs
@@ -1,23 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Assignment1 : ProcessingLite.GP21
 {
-    public int curves = 5;
+    public int curves = 15;
+
     public Transform start;
     public Transform middle;
     public Transform end;
-
     public float startX = 9.337861f;
     public float startY = 5;
 
-    void Start()
-    {
-        
-    }
+    public Color color1;
+    public Color color2;
+    public Color color3;
 
     void Update()
     {
@@ -28,6 +28,8 @@ public class Assignment1 : ProcessingLite.GP21
 
     void WriteName()
     {
+        Stroke(255, 255, 255);
+
         // K
         Line(1, 7, 1, 5);
         Line(1, 6, 2, 7);
@@ -56,13 +58,31 @@ public class Assignment1 : ProcessingLite.GP21
 
     void DrawParabolicCurve()
     {
+        Color color = new Color(1, 1, 1);
+        int colorNumber = 1;
         for (int i = 0; i < curves + 1; i++)
         {
-            //Vector3 test1 = (middle.position - start.position) / curves;
-            //Vector3 test2 = (end.position - middle.position) / curves;
-            Vector2 lineStart = start.position + (middle.position - start.position) / curves * i;
-            Vector2 lineEnd = middle.position + (end.position - middle.position) / curves * i;
+            if (i % 3 == 0)
+            {
+                // Would put a switch here but intellisense didn't like my syntax for some reason so this'll have to do
+                if (colorNumber == 1)
+                    color = color1;
+                else if (colorNumber == 2)
+                    color = color2;
+                else if (colorNumber == 3)
+                {
+                    color = color3;
+                    colorNumber = 0;
+                }
+                colorNumber++;
+            }
 
+            Vector3 test1 = (middle.position - start.position) / curves;
+            Vector3 test2 = (end.position - middle.position) / curves;
+            Vector2 lineStart = start.position + test1 * i;
+            Vector2 lineEnd = middle.position + test2 * i;
+
+            Stroke(255 * Mathf.RoundToInt(color.r), 255 * Mathf.RoundToInt(color.g), 255 * Mathf.RoundToInt(color.b));
             // For some reason the camera change X and Y positions at start so adjust start drawing points accordingly
             Line(lineStart.x + startX, lineStart.y + startY, lineEnd.x + startX, lineEnd.y + startY);
         }
